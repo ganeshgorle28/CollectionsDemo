c3ac61e2a42179fc3d468f0383674d5dfdbb4a65
ganeshgorle <gnshgorle@gmail.com>
2023-01-13 20:41:51 +0530

    added file for arraylist

---
+++ src/com/list/Sample.java
@@ -0,0 +1,18 @@
+package com.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Mumbai");
+		list.add("Pune");
+		list.add("Banglore");
+		list.add("Khamgaon");
+		System.out.println(list);
+	}
+
+}
