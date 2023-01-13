def2f9e42c3dd9cbd043fbf18065ee58fb2b5938
ganeshgorle <gnshgorle@gmail.com>
2023-01-13 20:50:15 +0530

    added linkedlist file

---
+++ src/com/list/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = new LinkedList<Integer>();
+		list.add(12);
+		list.add(34);
+		list.add(76);
+		System.out.println(list);
+	}
+
+}
